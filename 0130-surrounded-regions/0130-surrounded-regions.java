c071b6bcf9213344701a783b3ef04fa38df10e20
Rupesh Vishwakarma <116112162+rupeshh132@users.noreply.github.com>
2026-09-25 22:13:42 +0530

    Time: 1 ms (99.78%), Space: 47.9 MB (38.7%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        // 1. Traverse boundary cells; mark all 'O's connected to boundary as '#'
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+
+        // 2. Flip remaining unvisited 'O's to 'X' (captured), and '#' back to 'O' (escaped)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != 'O') {
+            return;
+        }
+
+        // Mark as non-surrounded
+        board[r][c] = '#';
+
+        // Explore all 4 orthogonal neighbors
+        dfs(board, r + 1, c);
+        dfs(board, r - 1, c);
+        dfs(board, r, c + 1);
+        dfs(board, r, c - 1);
+    }
+}
